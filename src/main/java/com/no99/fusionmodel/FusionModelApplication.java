3a366ab898602aeaae02142225e3d28cd7e9b0ac
No-99-Tongji <3167937401@qq.com>
2025-10-15 10:25:03 +0800

    Initial commit

---
+++ src/main/java/com/no99/fusionmodel/FusionModelApplication.java
@@ -0,0 +1,13 @@
+package com.no99.fusionmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/no99/fusionmodel/FusionModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no99.fusionmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
